0a10d1ad2b3dde745551bfbf3d87584bc3ac758d
tippydoor <rusj250505@gmail.com>
2021-01-01 20:33:19 +0300

    Update curator.hpp

--- src/ls_mission.altis/curator.hpp
+++ src/ls_mission.altis/curator.hpp
@@ -33,5 +33,6 @@ class Curators
         ,"76561198309313248"//Rock (Event)
         ,"76561198297807448"//Redad (Event)
         ,"76561198882089189"//Martinez (Event)
+		,"76561199071806700"//Warthog (Event)
 	};
 };
