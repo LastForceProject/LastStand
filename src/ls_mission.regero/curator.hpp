0f0e918e309900da08b7deed29318584f0166881
Demetrio7 <dima19842003.ru@mail.ru>
2021-01-08 13:09:37 +0400

    Выписал

--- src/ls_mission.regero/curator.hpp
+++ src/ls_mission.regero/curator.hpp
@@ -24,7 +24,6 @@ class Curators
         ,"76561198308824143"//Сoodich (Event)
         ,"76561198008400185"//Bombaster (Event)
         ,"76561199071806700"//Warthog (Event) ARS
-        ,"76561198141641033"//Pimpa (Event_Naim)
         ,"76561198064482123"//Dust (Event_Naim)
         ,"76561198335619075"//Anvil (Event_Naim)
         ,"76561198062501233"//Lore (Event_Naim)
