0c1d5740103ae0187fd348c454d11b7b85748a6b
DermoSobaki <71284027+DermoSobaki@users.noreply.github.com>
2021-01-13 13:41:08 +0300

    Поправил список
    КАСТЫЛЬ МАЗАФАКА...... НЕ ПИШИ БОЛЬШЕ DONATE слово ёпта

--- src/ls_mission.regero/curator.hpp
+++ src/ls_mission.regero/curator.hpp
@@ -9,27 +9,27 @@ class Curators
         ,"76561198288271495"//KapayJl (Tech)
         ,"76561199028516754"//Desire (Tech)
         ,"76561198121927537"//Morket (Tech) - Host
+	,"76561198227583576"//Filin (Сur_Naim)
         ,"76561198920070747"//Quantos (CURATOR_BSO)
-        ,"76561198166050664"//Foxxy (Curator_Event)
+        ,"76561198166050664"//Foxxy (Curator_Event v2.0)
         ,"76561198209489483"//Ars (Curator_Event)
-        ,"76561198227583576"//Filin (Сur_Naim)
         ,"76561198307319806"//Castel (Event)
         ,"76561198247446122"//КапитанБля (Event) - ARS
-        ,"76561198253231135"//BKA (Event Jedi)
+        ,"76561198253231135"//BKA (Event)
         ,"76561198367722284"//Bese(Event) - ARS
-        ,"76561198033904243"//Jisai (Donate_Zeus)
         ,"76561198374549023"//бобажив(игл)(Event)
         ,"76561198160053406"//Oni (Event)
         ,"76561198308824143"//Сoodich (Event)
-        ,"76561199071806700"//Warthog (Event) ARS
-        ,"76561198335619075"//Anvil (Event_Naim)
-        ,"76561198062501233"//Lore (Event_Naim)
-        ,"76561198403064350"//Smetanka (Event_Naim)
+        ,"76561199071806700"//Warthog (Event) - ARS
+        ,"76561198335619075"//Anvil (Event)
+        ,"76561198062501233"//Lore (Event)
+        ,"76561198403064350"//Smetanka (Event)
         ,"76561198093467695"//Nelson (Event) - ARS
         ,"76561198341622503"//Draxler (Event)
-		,"76561198082240166"//aikboy (Event) - ARS
-		,"76561198133650658"//Randez (Event_Naim)
-        ,"76561198102983006"//liming (Event Jedishit)
-		,"76561198378684428"//Myrr (Event_Naim)
+	,"76561198082240166"//aikboy (Event) - ARS
+	,"76561198133650658"//Randez (Event)
+        ,"76561198102983006"//liming (Event)
+	,"76561198378684428"//Myrr (Event)
+        ,"76561198033904243"//Jisai (D_Zeus)
 	};
 };
