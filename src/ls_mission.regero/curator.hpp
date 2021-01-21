e617934b64283cfe5152699469d2b7e9cb9b8a9e
Demetrio7 <dima19842003.ru@mail.ru>
2021-01-21 22:24:07 +0400

    Обновил зевсов

--- src/ls_mission.regero/curator.hpp
+++ src/ls_mission.regero/curator.hpp
@@ -32,6 +32,7 @@ class Curators
         ,"76561198335619075"//Anvil (Event)
         ,"76561198194246470"//Bluber (Event)
 		,"76561198378684428"//Myrr (Event)
+		,"76561198796417870"//German (Event)
         ,"76561198139144056"//Farsight (Event)
         ,"76561198033904243"//Jisai (D_Zeus)
 	};
