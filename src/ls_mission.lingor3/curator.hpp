d190c505b3227670bd2de0bef65cac8a687f4969
Demetrio7 <dima19842003.ru@mail.ru>
2021-02-03 15:53:17 +0400

    -

--- src/ls_mission.lingor3/curator.hpp
+++ src/ls_mission.lingor3/curator.hpp
@@ -31,7 +31,6 @@ class Curators
         ,"76561198194246470"//Bluber (Event)
 		,"76561198796417870"//German (Event)
         ,"76561198139144056"//Farsight (Event) 
-        ,"76561198092314817"//Zander (Event) 
         ,"76561198130795873"//Silv (Event)
         ,"76561198033904243"//Jisai (D_Zeus)
 	};
